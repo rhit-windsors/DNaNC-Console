68d5e0543954763731c0606b3214278a5458bb6c
RileyFW <windsors@rose-hulman.edu>
2024-05-16 00:02:34 -0400

    Initial commit

---
+++ DNaNC-Console/Objects/Node.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Numerics;
+using System.Runtime.InteropServices.JavaScript;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DNaNC_Client.Objects
+{
+    public class Node
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public UInt64 Id { get; set; }
+
+        public Node(string host, int port)
+        {
+            Host = host;
+            Port = port;
+            //Set Id
+            Id = GenerateId(Host, Port);
+        }
+        
+        private UInt64 GenerateId(string host, int port)
+        {
+            //Generate the id for the node
+            var hash = SHA1.HashData(Encoding.UTF8.GetBytes($"{host}:{port}"));
+            return BitConverter.ToUInt64(hash, 0);
+        }
+        
+        //DONE: Implement Compare
+        public static int Compare(Node? x, Node? y)
+        {
+            if (x == null && y == null)
+            {
+                return 0;
+            }
+            if (x == null)
+            {
+                return -1;
+            }
+            if (y == null)
+            {
+                return 1;
+            }
+            return x.Id.CompareTo(y.Id);
+        }
+        
+        //DONE: Implement Equals
+        public static bool Equals(Node? x, Node? y)
+        {
+            if (x == null && y == null)
+            {
+                return true;
+            }
+
+            if (x == null || y == null)
+            {
+                return false;
+            }
+
+            return x == y;
+        }
+
+        //TODO: Implement ToString
+        
+        //TODO: Implement GetHashCode
+    }
+}
---
+++ DNaNC-Console/Program.cs
@@ -0,0 +1,75 @@
+﻿//Ask user for port
+
+using DNaNC_Console.Services;
+using FileShare = DNaNC_Console.Services.FileShare;
+
+Console.Write("Enter the port to host this node from: ");
+var port = int.Parse(Console.ReadLine()!);
+
+//Ask which node to join
+Console.Write("Enter the host of the node to join: ");
+var host = Console.ReadLine()!;
+Console.Write("Enter the port of the node to join: ");
+var joinPort = Console.ReadLine();
+
+//Join the network
+if(host == "" || joinPort == "")
+{
+    //This is the first node in the network
+    NodeManager.InitNetwork(port);
+}
+else
+{
+    NodeManager.Join(host, int.Parse(joinPort!), port);
+}
+
+//Start listening
+_ = Task.Run(NodeServer.Listen);
+
+while (true)
+{
+    Console.Write("-> ");
+    var input = Console.ReadLine();
+    if (input == "exit")
+    {
+        NodeManager.Leave();
+        break;
+    }
+    if (input == "print")
+    {
+        Console.WriteLine($"Local Node: {NodeManager.LocalNode.Host}:{NodeManager.LocalNode.Port}");
+        Console.WriteLine($"Successor: {NodeManager.Successor?.Host}:{NodeManager.Successor?.Port}");
+        Console.WriteLine($"Predecessor: {NodeManager.Predecessor?.Host}:{NodeManager.Predecessor?.Port}");
+        Console.WriteLine($"Successor's Successor: {NodeManager.SuccessorSuccessor?.Host}:{NodeManager.SuccessorSuccessor?.Port}");
+        Console.WriteLine($"Predecessor's Predecessor: {NodeManager.PredecessorPredecessor?.Host}:{NodeManager.PredecessorPredecessor?.Port}");
+    }
+
+    if (input.Split(" ")[0] == "query")
+    {
+        var fileName = input.Split(" ")[1].Trim();
+        NodeManager.Query(fileName, NodeManager.LocalNode);
+    }
+
+    if (input.Split(" ")[0] == "share")
+    {
+        //Share the file
+        FileShare.ShareFile(input.Split(" ")[1]);
+    }
+    
+    if(input == "list")
+    {
+        //List the files
+        Console.WriteLine(FileShare.ListSharedFiles());
+    }
+
+    if (input == "found")
+    {
+        Console.WriteLine(FileShare.ListFoundFiles());
+    }
+
+    if (input.Split(" ")[0] == "get")
+    {
+        var index = int.Parse(input.Split(" ")[1]);
+        NodeManager.RequestFile(0);
+    }
+}
